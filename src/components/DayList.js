de68f8cb5c833f8ae6a2f2f39b32bcdd0727f831
bybang <bybang26@gmail.com>
2022-01-20 07:33:56 -0500

    Added Appoinment directory and story. minor refactor in Application.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,6 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  console.log(props);
   const listDays = props.days.map((day) => {
     return (
       <ul>
@@ -10,8 +9,8 @@ export default function DayList(props) {
           key={day.id}
           name={day.name}
           spots={day.spots}
-          selected={day.name === props.day}
-          setDay={props.setDay}
+          selected={day.name === props.value}
+          setDay={props.onChange}
         />
       </ul>
     );
