04faddd77418129540f43d7fff0998e82c15b2dc
bybang <bybang26@gmail.com>
2022-01-23 20:20:22 -0500

    Added hooks for the App, and installed react-hook library to check it

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+    replace ? setHistory(history) : setHistory((prev) => [...prev, newMode]);
+  };
+
+  const back = () => {
+    if (history.length === 1) return;
+
+    let copyHistory = history;
+    copyHistory.pop();
+    console.log("copyHistroy arrray: ", copyHistory);
+    setMode(copyHistory[copyHistory.length - 1]);
+    setHistory(copyHistory);
+  };
+
+  return { mode, transition, back };
+}
